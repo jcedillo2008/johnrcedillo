ef02468fea6c80cdee45b1bc3e78becee6ced648
jcedillo2008 <john.cedillo.2008@outlook.com>
2016-02-09 23:30:28 -0600

    Create view_cbd_clock_home.php

---
+++ view_cbd_clock_home.php
@@ -0,0 +1,18 @@
+<section class="<?php echo $scheme_name ?> main-opening" id="site-entry">
+	<article class="cbd-content">
+		<div id="clock" class="dark">
+      <div class="display">
+        <div class="weekdays"></div>
+        <div class="ampm"></div>
+        <div class="alarm"></div>
+        <div class="digits"></div>
+      </div>
+    </div>
+
+    <div class="button-holder">
+      <a class="button">Switch Theme</a>
+    </div>
+    <script type="text/javascript"></script>
+  </body>
+	</article>
+</section>
